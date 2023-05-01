aad8e311ae5d92c341ef6a74aa819230f90c6be6
James G <github@ckplace.org>
2023-04-30 21:39:54 -0400

    Added licensing information

--- src/main/java/org/ckplace/FibbonachiHeapDemo/Main.java
+++ src/main/java/org/ckplace/FibbonachiHeapDemo/Main.java
@@ -1,5 +1,21 @@
+/**
+ *  A Simple demonstration of a Fibonnachi Heap
+ *     Copyright (C) 2023 James Green
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.ckplace.FibbonachiHeapDemo;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
